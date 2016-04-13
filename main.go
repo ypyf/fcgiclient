258968495b8b859b5df1d3e5265cb360ddd96224
ypyf <t34@qq.com>
2016-04-14 05:44:06 +0800

    import project files

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	"fcgiclient/fastcgi"
+)
+
+func main() {
+	reqParams := "name=value"
+
+	env := make(map[string]string)
+	env["SCRIPT_FILENAME"] = "/home/yanfen/test.php"
+	env["SERVER_SOFTWARE"] = "go / fcgiclient "
+	env["REMOTE_ADDR"] = "127.0.0.1"
+	env["QUERY_STRING"] = reqParams
+
+	// fcgi, err := fcgiclient.New("tcp", "127.0.0.1:9000")
+	// if err != nil {
+	// 	log.Println("err:", err)
+	// }
+
+	fcgi, err := fastcgi.Dial("tcp", "127.0.0.1:9000")
+	if err != nil {
+		log.Println("err:", err)
+		return
+	}
+
+	resp, err := fcgi.Get(env)
+	if err != nil {
+		log.Println("err:", err)
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("err:", err)
+	}
+	log.Println("content:", string(content))
+}
